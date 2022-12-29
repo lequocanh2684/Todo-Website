265c367d458a985a3b1e527ba144edc83c98c470
lequocanh2684 <anh.lq192684@sis.hust.edu.vn>
2022-12-29 09:13:16 +0700

    first code

---
+++ Todo/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Todo.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
---
+++ Todo/Models/ToDo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace Todo.Models
+{
+	public class ToDo
+	{
+		string? Id { get; set; }
+
+		string? Titile { get; set; }
+
+		bool IsCompleted { get; set; }
+
+		public ToDo()
+		{
+		}
+	}
+}
+
