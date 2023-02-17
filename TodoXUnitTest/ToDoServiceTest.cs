e278e7b7d173009778740442a1f5cfc5355ed3df
anhld2001 <anhsteve2001@gmail.com>
2023-02-17 13:26:42 +0700

    xUnit_Testing

--- TodoXUnitTest/ToDoServiceTest.cs
+++ TodoXUnitTest/ToDoServiceTest.cs
@@ -11,6 +11,7 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using FluentAssertions;
+using Microsoft.CodeAnalysis.CSharp;
 
 namespace TodoXUnitTest
 {
@@ -52,6 +53,68 @@ namespace TodoXUnitTest
 			var result = await _toDoServiceTest.GetListToDo("37f96471-a1c2-4a59-8b28-41671e3c1609");
 			CollectionAssert.Equals(tempListToDoTest, result);
 		}
-	}
+		[Fact]
+		public async void AddToDo_ReturnSuccess()
+		{
+			var tempListToDoTest = new List<ToDoDTO>
+			{
+				new ToDoDTO()
+				{
+					Id = Guid.Parse("7a7cbb2f-1173-44b5-8f78-84c4f4202947"),
+					Title = "test3",
+					IsCompleted = false
+				},
+				new ToDoDTO()
+				{
+					Id = Guid.Parse("7a7cbb2f-1173-44b5-8f78-84c4f4202944"),
+					Title = "test4",
+					IsCompleted = false
+				}
+			};
+            var _toDoServiceTest = new ToDoService(_context.Object, _mapper);
+			var _newToDo = new ToDoForm()
+			{
+				Title= "test4",
+				IsCompleted = false,
+			};
+			await _toDoServiceTest.AddNewToDo(_newToDo, "37f96471-a1c2-4a59-8b28-41671e3c2749");
+            var result = await _toDoServiceTest.GetListToDo("37f96471-a1c2-4a59-8b28-41671e3c2749");
+			CollectionAssert.Equals(tempListToDoTest, result);
+        }
+
+		[Fact]
+		public async void EditToDo_ReturnSuccess()
+		{
+			var tempToDoTest = new ToDoDTO()
+			{
+				Id = Guid.Parse("7a7cbb2f-1173-44b5-8f78-84c4f4202947"),
+				Title = "test_edited",
+				IsCompleted = false
+			};
+            var _toDoServiceTest = new ToDoService(_context.Object, _mapper);
+			var check = _toDoServiceTest.EditExistToDo(tempToDoTest, Guid.Parse("7a7cbb2f-1173-44b5-8f78-84c4f4202947")).IsCompletedSuccessfully;
+			check.Should().BeTrue();
+			var result = await _toDoServiceTest.GetListToDo("37f96471-a1c2-4a59-8b28-41671e3c2749");
+			CollectionAssert.Equals(_toDoServiceTest, result);
+        }
+
+		[Fact]
+		public async void DeleteToDo_ReturnSuccess()
+		{
+			/*var tempToDoTest = new ToDoDTO()
+			{
+                Id = Guid.Parse("7a7cbb2f-1173-44b5-8f78-84c4f4263677"),
+                Title = "test_edited",
+                IsCompleted = false
+            };*/
+			var _toDoServiceTest = new ToDoService(_context.Object, _mapper);
+			var check = _toDoServiceTest.DeleteToDo(Guid.Parse("7a7cbb2f-1173-44b5-8f78-84c4f4228471")).IsCompletedSuccessfully;
+			check.Should().BeTrue();
+			var check1 = _context.Object.ToDoList.Find(Guid.Parse("7a7cbb2f-1173-44b5-8f78-84c4f4228471"));
+			check1.IsDeleted.Should().BeTrue();
+/*			var result = await _toDoServiceTest.GetListToDo("37f96471-a1c2-4a59-8b28-41671e3c1609");
+*//*			CollectionAssert.Equals(_toDoServiceTest, result);
+*/		}
+    }
 }
 
